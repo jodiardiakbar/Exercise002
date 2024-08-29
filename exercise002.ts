1d1ad90c64b1c64d01e953bd59d5a016023fd681
jodiardiakbar <jodiardi.akbar@gmail.com>
2024-08-29 16:08:19 -0400

    feat: add exercise002.ts with all typescript parts

---
+++ exercise002.ts
@@ -0,0 +1,27 @@
+// number checker
+let numberToCheck: number = 26;
+if (numberToCheck % 2 === 0) {
+  console.log(`even number`);
+} else {
+  console.log(`odd number`);
+}
+
+// prime number
+
+// sum 1 to n
+let n: number = 3;
+let hasil: number = 0;
+for (let i: number = 0; i <= n; i++) {
+  hasil += i;
+}
+console.log(hasil);
+
+// factorial mumber
+let factorialNumber: number = 4;
+let result: number = 1;
+for (let i: number = 1; i <= factorialNumber; i++) {
+  result = result * i;
+}
+console.log(result);
+
+// fibonacci
